a2759e7a864b2f11671294376084d2c89af0fa9a
ahmed-fareeq-0 <ahmed.fareeq.0@gmail.com>
2022-05-29 21:44:34 +0300

    Create layout for login page and registration page

---
+++ js/register.js
@@ -0,0 +1,11 @@
+// import input register 
+const register_userName = document.querySelector("#register-input-userName");
+const register_email = document.querySelector("#register-input-email");
+const register_psd = document.querySelector("#register-input-psd");
+const register_psd2 = document.querySelector("#register-input-psd2");
+const register_btn = document.querySelector("#registr-btn");
+
+
+register_btn.addEventListener("click", (e) => {
+   
+})
