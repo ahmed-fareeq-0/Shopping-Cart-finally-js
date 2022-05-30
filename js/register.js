eef7f531e02f57c8cb60026debd9780c108a875e
ahmed-fareeq-0 <ahmed.fareeq.0@gmail.com>
2022-05-30 20:07:14 +0300

    Create an account on the registration page and save the data in local storage

--- js/register.js
+++ js/register.js
@@ -1,11 +1,31 @@
 // import input register 
-const register_userName = document.querySelector("#register-input-userName");
-const register_email = document.querySelector("#register-input-email");
-const register_psd = document.querySelector("#register-input-psd");
-const register_psd2 = document.querySelector("#register-input-psd2");
-const register_btn = document.querySelector("#registr-btn");
+const reg_userName = document.querySelector("#register-input-userName");
+const reg_email = document.querySelector("#register-input-email");
+const reg_psd = document.querySelector("#register-input-psd");
+const reg_psd2 = document.querySelector("#register-input-psd2");
+const reg_btn = document.querySelector("#registr-btn");
 
 
-register_btn.addEventListener("click", (e) => {
-   
+reg_btn.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    if (reg_userName.value === "" || reg_email.value === "" || reg_psd.value === "" || reg_psd2.value === "") {
+        alert("please full data")
+
+    } else if (reg_psd.value !== reg_psd2.value) {
+        alert("Wrong password matching")
+
+    } else {
+
+        localStorage.setItem("userName", reg_userName.value);
+        localStorage.setItem("email", reg_email.value);
+        localStorage.setItem("psd", reg_psd.value);
+        localStorage.setItem("psd2", reg_psd2.value);
+
+        setTimeout(() => { window.location = "login.html" }, 1500)
+    }
 })
+
+
+
+
