2ae0df2850b6b095b7240e761b076b8d57ee7301
ahmed-fareeq-0 <ahmed.fareeq.0@gmail.com>
2022-06-15 22:22:40 +0300

    remove product in cart 2

--- js/carts.js
+++ js/carts.js
@@ -1,15 +1,17 @@
 let DomCartProducts = document.querySelector("#DomCartProducts");
-let getProduct = localStorage.getItem("products")
 
 
-if(getProduct){
-    let items = JSON.parse(getProduct)
-    DrawproductsUi(items)
-}else {
-    console.log("no");
-}
 
-function DrawproductsUi(products) {
+// if(getProduct){
+//     let items = JSON.parse(getProduct)
+//     DrawproductsUi(items)
+// }else {
+//     console.log("no");
+// }
+
+function DrawproductsUi(allProducts = []) {
+
+    let products = JSON.parse(localStorage.getItem("products"))  || allProducts ;  
     let CartProducUi = products.map((item) => {
         return `
             <div class="card">
@@ -27,15 +29,38 @@ function DrawproductsUi(products) {
     });
     DomCartProducts.innerHTML = CartProducUi ;
 }
+DrawproductsUi()
+
+
+
+let noProductInCart = document.getElementById("noProductInCart")
+// If the cart page is empty
+if(JSON.parse(localStorage.getItem("products")).length === 0){
+    noProductInCart.innerHTML = "no products"
+}
+
 
 
 // remove product in cart
 function Removefromcart (id) {
+    let getProduct = localStorage.getItem("products")
     if (getProduct) {
         let items = JSON.parse(getProduct);
         let filteredItems = items.filter((item) => item.id !== id);
         localStorage.setItem("products", JSON.stringify(filteredItems));
         DrawproductsUi(filteredItems)
     }
+
+    if(JSON.parse(localStorage.getItem("products")).length === 0){
+        noProductInCart.innerHTML = "no products" 
+    }
 }
+
+
+
+
+
+
+
+
 
